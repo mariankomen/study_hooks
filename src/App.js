02cb643f7c312014f4a5bbca5efc8953f3436a85
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2021-01-24 22:32:39 +0200

    chng title by useState

--- src/App.js
+++ src/App.js
@@ -3,30 +3,15 @@ import './App.css';
 import Comp from "./components/components";
 
 function App() {
+    const [style, setStyle] = useState('default')
 
-    const [data, setData] = useState([
-        {id: 1, name: 'Ivan', age: 23},
-        {id: 2, name: 'Petro', age: 15},
-        {id: 3, name: 'Stepan', age: 65},
-    ])
-    const addItem = () => {
-        const item = {id: 4, name: 'Igot', age: 20}
-        let newarr = data.push(item)
-        console.log(data)
-        return{
-            data: newarr
-        }
-
-    }
+    document.title = style
 
     return (
         <div className="App">
-            <Comp data={data}/>
-        <button
-        onClick={addItem}>
-            Click
-        </button>
-
+        <button onClick={()=>setStyle('Hello')}>Hello</button>
+        <button onClick={()=>setStyle('World')}>World</button>
+            <p>{document.title}</p>
         </div>
     );
 }
